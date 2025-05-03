54549412756d347ea6d29629c3b8063bdffe1fbd
Bassel W. Ibrahim <bassel.walid04@gmail.com>
2025-05-03 19:02:06 +0300

    feat: function dec param parsing

--- parser.cpp
+++ parser.cpp
@@ -1,5 +1,6 @@
 ﻿#include "iostream"
 #include "string"
+#include "parser.h"
 using namespace std;
 static void nextToken() { int x = 1; };
 enum TokenType {
@@ -30,7 +31,10 @@ enum TokenType {
   MULTI_LINE_COMMENT_END,
   STRUCT,
   BREAK,
-  IDENTIFIER
+  IDENTIFIER,
+  COMMA,
+  COLON,
+  SEMICOLON
 };
 
 bool isDataType(TokenType token) {
@@ -70,21 +74,74 @@ void parseIncludes() {
   // and the content of the file will be dumbed in the token stream.
 }
 
+/*8. `params → param-list | NOReturn | ε`
+9. `param-list → param p_list`
+10. `p_list -> , param p_list | e`
+11. `param → type-specifier ID`*/
+
+void parseParam() {
+    if (isDataType(current_token.type)) {
+        nextToken();
+    }
+    if (current_token.type == IDENTIFIER) {
+        nextToken();
+        return;
+    }
+    else {
+        throwError();
+    }
+}
+
+void parsePList() {
+    if (current_token.type == CLOSE_BRACKET) {
+        return;
+    } else if (current_token.type == COMMA) {
+        nextToken();
+        parseParam();
+        parsePList();
+        return;
+    }
+    else {
+        throwError();
+    }
+}
+
+void parseParamList() {
+    parseParam();
+    parsePList();
+}
+
+void parseParams() { 
+    nextToken();
+    if (isDataType(current_token.type))
+    {
+        parseParamList();
+    }
+    else if (current_token.type == VOID || current_token.type == CLOSE_BRACE) {
+        return;
+    }
+    else {
+        throwError();
+    }
+}
+
+void parseCompoundStmt() {
+};
 // 7. `fun-declaration → type-specifier ID ( params ) compound-stmt | comment
 // type-specifier ID`
 void parseFunDec() {
   if (current_token.type == OPEN_BRACE) {
     parseParams();
-    if (current_token.type == OPEN_BRACE) {
+    if (current_token.type == OPEN_BRACKET) {
       parseCompoundStmt();
-    }
+    } 
   }
 }
-
 // 2. `declaration-list → declaration d_list`
 // 3. `d_list->declaration d_list | e`
 // 4. declaration → var - declaration |fun - declaration
-void parseDeclarationList() {//need to figure out how to handle the recursive part of recursive descent lol}
+void parseDeclarationList() {//need to figure out how to handle the recursive part of recursive descent lol
+}
 void parseDeclaration() {
   if (isDataType(current_token.type)) {
     nextToken();
