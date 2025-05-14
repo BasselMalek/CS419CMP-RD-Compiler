6950813d8e07cf652692812dd3d0510acf7cdf0d
Bassel W. Ibrahim <bassel.walid04@gmail.com>
2025-05-14 21:42:31 +0300

    move headers to include

--- include/lexer.h
+++ include/lexer.h
@@ -1,16 +1,34 @@
 #ifndef LEXER_H
 #define LEXER_H
 
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include "Token.h"
 
-class lexer
-{
-    public:
-        lexer();
-        virtual ~lexer();
+class Lexer {
+public:
+    explicit Lexer(const std::string& sourceCode);
+    std::vector<Token> tokenize();
 
-    protected:
+private:
+    std::string source;
+    size_t pos;
+    int line;
+    std::unordered_map<std::string, std::string> keywords;
+    std::unordered_set<std::string> includedFiles;
 
-    private:
+    char peek(int n = 0);
+    char get();
+    void skipWhitespace();
+    TokenType TokenTypeFromString(const std::string& typeStr);
+    Token lexIdentifierOrKeyword();
+    Token lexNumber();
+    Token lexString();
+    Token lexChar();
+    Token lexOperatorOrPunctuation();
+    std::vector<Token> lexComment();
 };
 
-#endif // LEXER_H
+#endif
--- include/parser.h
+++ include/parser.h
@@ -1,129 +1,76 @@
-#ifndef PARSER_H
-#define PARSER_H
-
-
-class parser
-{
-    public:
-        parser();
-        virtual ~parser();
-
-    #pragma once
+#pragma once
 
 #include <string>
 #include <vector>
+#include <fstream>
+#include "Token.h"
 
-// Token type enumeration
-enum TokenType {
-  CLEAR,
-  CONDITION,
-  INTEGER,
-  SINTEGER,
-  CHARACTER,
-  STRING,
-  FLOAT,
-  SFLOAT,
-  VOID,
-  LOOP,
-  RETURN,
-  ARITHMETIC_OP,
-  LOGIC_OP,
-  RELATIONAL_OP,
-  ASSIGNMENT_OP,
-  ACCESS_OP,
-  BRACE,
-  CONSTANT,
-  QUOTATION_MARK,
-  INCLUSION,
-  STRUCT,
-  BREAK,
-  IDENTIFIER,
-  COMMA,
-  COLON,
-  SEMICOLON
-};
-
-// Token structure
-struct Token {
-  TokenType type;
-  std::string value;
-  Token() = default;
-  Token(TokenType t, std::string val);
-};
-
-// Parser class declaration
 class Parser {
 private:
-  std::vector<Token> tokens;
-  Token current_token;
-  unsigned int token_index;
-  unsigned int line_count;
-  unsigned int error_count;
+    std::vector<Token> tokens;
+    Token current_token;
+    unsigned int token_index;
+    unsigned int line_count;
+    unsigned int error_count;
+    bool in_function_scope;
 
-  bool isDataType(TokenType token);
-  bool isStartOfStatement(TokenType type);
-  bool isStartsOfLine(TokenType token);
-  void nextToken();
-  void throwError();
+    bool isDataType(TokenType token);
+    bool isStartOfStatement(TokenType type);
+    bool isStartsOfLine(TokenType token);
+    void nextToken();
+    void throwError(std::ofstream& out);
 
-  // Declaration parsing methods
-  void parseDeclarations();
-  void parseDeclarationList();
-  void parseDeclaration();
-  void parseVarDec();
-  void parseTypeSpecifier();
-  void parseFunDec();
-  void parseParams();
-  void parseParamList();
-  void parsePList();
-  void parseParam();
+    void parseDeclarations(std::ofstream& out);
+    void parseDeclarationList(std::ofstream& out);
+    void parseDeclaration(std::ofstream& out);
+    void parseStructDec(std::ofstream& out);
+    void parseVarDec(std::ofstream& out);
+    void parseTypeSpecifier(std::ofstream& out);
+    void parseFunDec(std::ofstream& out);
+    void parseParams(std::ofstream& out);
+    void parseParamList(std::ofstream& out);
+    void parsePList(std::ofstream& out);
+    void parseParam(std::ofstream& out);
 
-  // Statement parsing methods
-  void parseCompoundStmt();
-  void parseLocalDecs();
-  void parseStmtList();
-  void parseStatement();
-  void parseExpressionStmt();
-  void parseSelectionStmt();
-  void parseIterationStmt();
-  void parseJumpStmt();
+    void parseCompoundStmt(std::ofstream& out);
+    void parseLocalDecs(std::ofstream& out);
+    void parseStmtList(std::ofstream& out);
+    void parseStatement(std::ofstream& out);
+    void parseExpressionStmt(std::ofstream& out);
+    void parseSelectionStmt(std::ofstream& out);
+    void parseIterationStmt(std::ofstream& out);
+    void parseJumpStmt(std::ofstream& out);
 
-  // Expression parsing methods
-  void parseExpression();
-  void parseIdAssign();
-  void parseSimpleExpression();
-  void parseRelop();
-  void parseAdditiveExpression();
-  void parseAdditiveExpressionPrime();
-  void parseAddOp();
-  void parseTerm();
-  void parseMulOp();
-  void parseTermPrime();
-  void parseFactor();
-  void parseCall();
-  void parseArgs();
-  void parseArgList();
-  void parseAList();
+    void parseExpression(std::ofstream& out);
+    void parseIdAssign(std::ofstream& out);
+    void parseSimpleExpression(std::ofstream& out);
+    void parseRelop(std::ofstream& out);
+    void parseAdditiveExpression(std::ofstream& out);
+    void parseAdditiveExpressionPrime(std::ofstream& out);
+    void parseAddOp(std::ofstream& out);
+    void parseTerm(std::ofstream& out);
+    void parseMulOp(std::ofstream& out);
+    void parseTermPrime(std::ofstream& out);
+    void parseFactor(std::ofstream& out);
+    void parseCall(std::ofstream& out);
+    void parseArgs(std::ofstream& out);
+    void parseArgList(std::ofstream& out);
+    void parseAList(std::ofstream& out);
 
-  // Number parsing methods
-  void parseNum();
-  void parseUnsignedNum();
-  void parseSignedNum();
-  void parsePosNum();
-  void parseNegNum();
-  void parseValue();
+    void parseNum(std::ofstream& out);
+    void parseUnsignedNum(std::ofstream& out);
+    void parseSignedNum(std::ofstream& out);
+    void parsePosNum(std::ofstream& out);
+    void parseNegNum(std::ofstream& out);
+    void parseValue(std::ofstream& out);
 
-  // Other utility methods
-  void parseComment();
-  void parseIncludeCommand();
-  void parseFName();
+    void parseComment(std::ofstream& out);
+    void parseIncludeCommand(std::ofstream& out);
+    void parseFName(std::ofstream& out);
 
 public:
-  Parser();
-  void setTokens(const std::vector<Token> &input_tokens);
-  int parse();
-  unsigned int getErrorCount() const;
-
+    Parser();
+    void setTokens(const std::vector<Token> &input_tokens);
+    int parse(std::ofstream& out);
+    unsigned int getErrorCount() const;
 };
-
-#endif // PARSER_H
